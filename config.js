407226a2786de274199a21c1c38a000f190edd15
Randy <randypriv@gmail.com>
2015-11-04 10:58:44 +0800

    分组逻辑重构

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  members: ["ac","get","jingying","yunqing","wgh","xuliang","randy", "cash", "harry", "ali"]
+}
--- random.js
+++ random.js
@@ -1,70 +1,41 @@
-var members = ["ac","get","jingying","yunqing","wgh","xuliang","randy", "cash", "harry", "ali"];
-var options = process.argv;
-var group_count = options[2];
+'use strict'
 
-var count = 0;
-var countTime = 10 * 1000;
-function rd(n,m){
-    var c = m-n+1;  
-    return Math.floor(Math.random() * c + n);
-}
+let utils = require('./utils');
 
-var display = setInterval( function (){
-  var index = rd(0,members.length-1);
-  console.log( '你会跟TA：' + members[index] + ' 一起吃饭？？' );
-  countTime = countTime - 200;
-  var checkPoint = countTime;
-  if( (checkPoint % 1000) == 0 ){
-    console.log( '\n 还有' + (countTime /1000)  + '秒 \n' );
-  }
-}, 200);
-
-setTimeout( function (){
-  display = clearInterval(display)
-  gogogo();
-}, countTime );
-
-
-///实际
-function gogogo(){
-
-    // 数组乱序
-    members = shuffle(members);
-    groups = new Array();
-    // 分组基数
-    base_count = members.length / group_count;
-    
-    var index = 0;
-    for(; index < group_count; index++) {
-        start = index * base_count;
-        end = start + base_count;
-        groups[index] = members.slice(start, end);
-    }
-
-    console.log('');
-    var group_num = 1;
-    for( item in groups ) {
-        console.log("食家", group_num, "组:");
-        group_members = groups[item]
-        for( x in group_members) {
-            console.log("    ", group_members[x]);
-        }
-        group_num++;
-    }
-
+class Random{
+  constructor(members){
+    this.members = members;
   }
 
-function shuffle(array) {
-    var m = array.length,
-        t, i;
-    // 如果还剩有元素…
-    while (m) {
-        // 随机选取一个元素…
-        i = Math.floor(Math.random() * m--);
-        // 与当前元素进行交换
-        t = array[m];
-        array[m] = array[i];
-        array[i] = t;
+  except(members){
+    for (let i = members.length - 1; i >= 0; i--) {
+      this.members.splice(this.members.indexOf(members[i]), 1);
     }
-    return array;
+    return this;
+  }
+
+  run(expectGroupCount){
+    let groupCount = expectGroupCount;
+    let memberCountPerGroup = Math.floor(this.members.length / expectGroupCount);
+    this.members = utils.shuffle(this.members);
+
+    let restMemberCount = 0;
+    if ( this.members.length % expectGroupCount ) {
+      groupCount++;
+      restMemberCount = this.members.length % expectGroupCount;
+    }
+
+    let group = [];
+    for (let i = 0; i < expectGroupCount; i++) {
+      group.push(this.members.slice(i * memberCountPerGroup, (i + 1) * memberCountPerGroup));
+    };
+
+    if (restMemberCount) {
+      group.push(this.members.slice(-restMemberCount));
+    };
+
+    return this;
+  }
 }
+
+module.exports = Random;
